67e0d01f7c5218e1bc0efc2f43c649d8d86e0c36
Lee So Young <cheerup0101@naver.com>
2019-10-30 19:39:50 +0900

    reference copy

--- 참조와복사.js
+++ 참조와복사.js
@@ -33,3 +33,48 @@ var 함수 = function() {};
 복사 = 함수;
 복사.abc = 'def';
 console.log(함수.abc); // 'def'
+
+//객체 복사하기
+/* var obj = {a:1 , b:2};
+var obj2 = {};
+obj2.a = obj.a;
+obj2.b = obj.b;
+console.log(obj2)// {a:1 , b:2}
+obj2.a = 3;
+console.log(obj1.a)// 1 */
+
+var obj = {a: 1 , b: 2 , c: 3};
+Object.keys(obj) // [a,b,c] 
+var obj2 = {};
+Object.keys(obj).forEach(function(key) {
+   obj2[key] = obj[key];
+});
+
+console.log(obj2) // {a:1 , b:2 , c:3}
+obj2.a = 4;
+console.log(obj.a) // 1
+
+//객체간 참조관계 알아보기
+//obj === obj2 결과가 true 이면 참조관계, 연결되어있다.
+
+//객체안의 객체가 있을때 (2단계 구조)
+//객체안 객체는 참조가 된다.
+var ob = {a: 1, b: {c: 1}};
+Object.keys(ob) // [a,b,c] 
+
+var obj3 = {};
+Object.keys(ob).forEach(function(key) {
+   obj3[key] = ob[key];
+});
+obj3.b.c = 8;
+console.log(ob.b.c) // 8로 바뀐다!
+
+//얕은복사 -- 참조 , 깊은복사 -- 복사
+
+
+
+
+
+
+
+
